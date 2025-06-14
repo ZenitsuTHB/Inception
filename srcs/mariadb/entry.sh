2aa21e053d11369eeac0b89157121719a94a0303
avolcy <avolcy@localhost.localdomain>
2025-06-14 09:15:59 -0400

    some things works but some others no

--- srcs/mariadb/entry.sh
+++ srcs/mariadb/entry.sh
@@ -8,34 +8,65 @@ mysql_config_file()
 	cat << EOF > /etc/my.cnf
 	[mysqld]
 	user=mysql
-	datadir=${MYSQL_DATADIR}
+	datadir=${MDB_DIR}
 	port=${MARIADB_PORT}
 	bind-address=0.0.0.0
 	socket=/run/mysqld/mysqld.sock
 EOF
 }
 
+create_database()
+{
+	db_password_file=/run/secrets/mdb_user_password
+	db_root_password_file=/run/secrets/mdb_root_password
+
+	if [ -f $db_password_file ] && [ -f $db_root_password_file ]; then
+
+	db_password=$(cat $db_password_file)
+	db_root_password=$(cat $db_root_password_file)
+
+cat << EOF > ${MDB_DIR}/init-db.sql
+	CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+
+	CREATE USER IF NOT EXISTS "${MYSQL_ROOT}"@"%" IDENTIFIED BY "$db_root_password";
+	ALTER USER 'root'@'localhost' IDENTIFIED BY "$db_root_password";
+	GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO "${MYSQL_ROOT}"@"%" WITH GRANT OPTION;
+
+	CREATE USER IF NOT EXISTS "${MYSQL_USER}"@"localhost" IDENTIFIED BY "$db_password";
+	GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO "${MYSQL_USER}"@"localhost";
+	CREATE USER IF NOT EXISTS "${MYSQL_USER}"@"%" IDENTIFIED BY "$db_password";
+	GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO "${MYSQL_USER}"@"%";
+
+	FLUSH PRIVILEGES;
+EOF
+
+	else
+		echo "$db_password_file or $db_root_password_file not found..."
+	fi
+}
+
+
 start_database()
 {
 
 	echo "Creating Database"
-	if [ -d "${MYSQL_DATADIR}/mysql" ]; then
+	if [ -d "${MDB_DIR}/mysql" ]; then
 		echo "Database already initialized, starting MariaDB..."
 	else
 
-	mysql_install_db --user=mysql --datadir=${MYSQL_DATADIR} > /dev/null 2>&1
+	mysql_install_db --user=mysql --datadir=${MDB_DIR} > /dev/null 2>&1
 
-	mysqld --datadir=${MYSQL_DATADIR} &
+	mysqld --datadir=${MDB_DIR} &
 
 	while ! mysqladmin ping --silent; do
 	    echo "Waiting for Mariadb..."
 	    sleep 1
+	done
 
 	create_database
 
-	mysql -u root -p"$db_root_password" < ${MYSQL_DATADIR}/init-db.sql > /dev/null 2>&1
+	mysql -u root -p"$db_root_password" < ${MDB_DIR}/init-db.sql > /dev/null 2>&1
 	mysqladmin shutdown -u root -p"$db_root_password"
-	done
 fi
 	
 echo "Database Created!"
@@ -54,16 +85,19 @@ add_group()
 	if  ! getent passwd "$user" > /dev/null 2>&1; then
 		adduser -S -D -H -s /sbin/nologin -g $group $user;
 	fi
+	mkdir -p /run/mysqld
 	chown -R $user:$group $dir	
+	chown -R $user:$group /run/mysqld	
 }
 
 init_mdb() {
 	add_group "mysql" "mysql" "/var/lib/mysql"
 	mysql_config_file 
-	exec $@ -f /dev/null
+	start_database
+	exec $@
 }
 
-if [ $1 = "tail" ];
+if [ $1 = "mysqld" ];
 then 
        init_mdb "$@"
 else
